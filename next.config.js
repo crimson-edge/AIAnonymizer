77322a9d36ffc45e76db1efe63577022a6d29e76
Crimson Edge Ventures <support@crimsonedgeventures.com>
2024-12-30 16:44:02 -0800

    Switch to static export with out directory

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  target: 'serverless',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  }
+}
 
 module.exports = nextConfig
